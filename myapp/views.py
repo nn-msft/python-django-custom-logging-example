e8d720c1486e4f3d1807e15823059a883511d9f3
nn-msft <v-nanaik@microsoft.com>
2020-12-22 18:26:02 -0500

    x commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,12 +4,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 def homePageView(request):
-    logger.log(logging.INFO,"Hello World Visited!")
+    
     logger.critical("Hello World Visited!-------CRITICAL LEVEL")
     return render(request, 'home/index.html', {'msg':'Hello World'})
 
 def helloPageView(request):
-    logger.log(logging.INFO,"More Hello")
+    
     logger.critical("More Hello-------CRITICAL LEVEL")
     return render(request,'home/hello2.html',{'msg':'More Hello'})
 
@@ -20,11 +20,12 @@ def jsonSample(request):
         'is_active': True,
         'count': 28
     }
-    logger.log(logging.INFO,"==========Inside Json Sample===========")
-    logger.log(logging.INFO,str(request.body))
-    logger.log(logging.INFO,"==========Data Contents==============")
-    logger.log(logging.INFO,str(data))
-    logger.log(logging.INFO,"=========Let's return===========")
+    logger.critical("==============jsonSample============")
+    logger.critical("==========Inside Json Sample===========")
+    logger.critical("=========="+str(request.body)+"===========")
+    logger.critical("==========Data Contents==============")
+    logger.critical("==========="+str(data)+"===============")
+    logger.critical("=========Let's return===========")
 
-    logger.critical("jsonSample-------CRITICAL LEVEL")
+    
     return JsonResponse(data)
