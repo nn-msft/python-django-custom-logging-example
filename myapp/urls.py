8408ef61707d35c1ac650942c2ea6271b8e076ad
nn-msft <v-nanaik@microsoft.com>
2020-12-22 16:25:37 -0500

    third commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import homePageView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', homePageView, name='home'),
+    path('/hello',helloPageView,name='hello')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,7 @@ logger = logging.getLogger(__name__)
 def homePageView(request):
     logger.debug("Hello World Visited!")
     return render(request, 'home/index.html', {'msg':'Hello World'})
+
+def helloPageView(request):
+    logger.debug("More Hello")
+    return render(request,'home/hello2.html',{'msg':'More Hello'})
