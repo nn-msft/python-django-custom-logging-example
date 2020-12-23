57fce39c4e0c851f427bb9a6517ef965e18efb9a
nn-msft <v-nanaik@microsoft.com>
2020-12-23 01:03:44 -0500

    x commit

--- myapp/views.py
+++ myapp/views.py
@@ -2,16 +2,16 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import logging
 
-#logger = logging.getLogger(__name__)
+
 logger = logging.getLogger('django')
 def homePageView(request):
     
-    logger.critical("Hello World Visited!-------CRITICAL LEVEL")
+    logger.critical("Hello World Visited!")
     return render(request, 'home/index.html', {'msg':'Hello World'})
 
 def helloPageView(request):
     
-    logger.critical("More Hello-------CRITICAL LEVEL")
+    logger.critical("More Hello")
     return render(request,'home/hello2.html',{'msg':'More Hello'})
 
 def jsonSample(request):
